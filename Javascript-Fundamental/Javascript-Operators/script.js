0154772af19117b5f59df218ad32100a6180a571
Dennis Jason <jasonden98@gmail.com>
2020-11-21 22:04:12 +0700

    Learn Javascript Operators

---
+++ Javascript-Fundamental/Javascript-Operators/script.js
@@ -0,0 +1,22 @@
+var angka1 = 10;
+var angka2 = 30;
+
+console.log(angka1 + angka2);
+
+angka1 += 1;
+
+console.log(angka1);
+
+//========================
+var namaPertama = "Dennis ";
+var namaTerakhir = "Jason";
+
+console.log(namaPertama + namaTerakhir);//Opetor Concate
+
+console.log(namaPertama + angka1);//angka1 dianggap string karena variabel di depannya adalah string
+
+console.log(namaPertama + (angka1 + angka2));// di dalam kurung di eksekusi terlebih dahulu
+
+console.log(angka2 * angka1);//perkalian
+
+console.log(angka2 / angka1);//pembagian
